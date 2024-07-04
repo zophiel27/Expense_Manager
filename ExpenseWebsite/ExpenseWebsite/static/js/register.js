5ad532795546d11a7b4ca132012afb0857d1887f
Zophiel Suleman <zophielsuleman27@gmail.com>
2024-07-04 21:51:58 +0500

    Email Validation + On Successful Validation Displays

--- ExpenseWebsite/ExpenseWebsite/static/js/register.js
+++ ExpenseWebsite/ExpenseWebsite/static/js/register.js
@@ -1,9 +1,16 @@
 const usernameField = document.querySelector("#usernameField");
-const feedbackArea = document.querySelector(".invalid-feedback");
+const feedbackArea = document.querySelector(".Username-invalid-feedback");
+const emailField = document.querySelector("#emailField");
+const EmailfeedbackArea = document.querySelector(".Emailinvalid-feedback");
+const usernameSuccessOutput = document.querySelector(".username-success-output");
+const emailSuccessOutput = document.querySelector(".email-success-output");
 
 // event handler to know when user types something
 usernameField.addEventListener("keyup", (e) => {
     const usernameValue = e.target.value; //what user types in
+
+    usernameSuccessOutput.style.display = 'block';
+    usernameSuccessOutput.textContent = `Checking ${usernameValue}`
 
     usernameField.classList.remove('is-invalid');
     feedbackArea.style.display = 'none';
@@ -16,13 +23,56 @@ usernameField.addEventListener("keyup", (e) => {
         .then((res) => res.json())
         .then((data) => {
             console.log('data', data);
+            usernameSuccessOutput.style.display = 'none';
             if(data.username_error) {
-                usernameField.classList.add('is-invalid');
+                usernameField.classList.add('is-invalid'); //display red border
                 feedbackArea.style.display = 'block';
                 feedbackArea.innerHTML = `<p>${data.username_error}</p>`;
                 
             }
         });
     }
+    else
+    {
+        usernameSuccessOutput.style.display = 'none';
+    }
     
 });
+
+
+emailField.addEventListener("keyup", (e) => {
+    const emailValue = e.target.value; //what user types in
+    
+    emailSuccessOutput.style.display = 'block';
+    emailSuccessOutput.textContent = `Checking ${emailValue}`
+    
+    emailField.classList.remove('is-invalid');
+    EmailfeedbackArea.style.display = 'none';
+
+    if (emailValue.length > 0) {
+        fetch("/auth/validate-email/", {
+            body: JSON.stringify({email: emailValue}), //send data to server in json format
+            method: 'POST',
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('data', data);
+            emailSuccessOutput.style.display = 'none';
+            if(data.email_error) {
+                emailField.classList.add('is-invalid'); //display red border
+                EmailfeedbackArea.style.display = 'block';
+                EmailfeedbackArea.innerHTML = `<p>${data.email_error}</p>`;
+                
+            }
+        });
+    }
+    else
+    {
+        emailSuccessOutput.style.display = 'none';
+    }
+    
+});
+
+
+
+// static files not getting refreshed: ctrl+shift+r to refresh
--- ExpenseWebsite/authentication/views.py
+++ ExpenseWebsite/authentication/views.py
@@ -3,6 +3,7 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
 class RegistrationView(View):
@@ -24,3 +25,20 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error': 'Sorry, this username is already taken. Please try another one'}, status=409)
         
         return JsonResponse({'username_valid': True})
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body) #contains everything entered by user
+        email = data['email']
+
+        # if its in a valid email format
+        # pipenv install validate-email
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        
+        # if username is already taken
+        # check database
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'Sorry, this email is already taken. Please try another one'}, status=409)
+        
+        return JsonResponse({'email_valid': True})
